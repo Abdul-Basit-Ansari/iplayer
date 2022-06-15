ac82b63a828b5ec8b9483e94b5ad0a894894569b
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-15 18:29:27 +0500

    set media and static files

--- player/settings.py
+++ player/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL="/media/"
-MEDIA_ROOT= BASE_DIR / 'media'
+# MEDIA_URL="/media/"
+# MEDIA_ROOT= BASE_DIR / 'media'
 STATIC_ROOT= os.path.join(BASE_DIR , 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
