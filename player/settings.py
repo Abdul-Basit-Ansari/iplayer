4e74cc9f6b98ecf4d08792dc361a878c42277190
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-15 17:29:05 +0500

    set staticfiles

--- player/settings.py
+++ player/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL="/media/"
 MEDIA_ROOT= BASE_DIR / 'media'
+STATIC_ROOT= os.path.join(BASE_DIR , 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- player/urls.py
+++ player/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("vplayer.urls")),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
