6583e7e1760ccf2d5ede182465aaddc22938c168
Abdul Basit <abdulbasitansari288@gmail.com>
2022-06-16 18:24:04 +0500

    add white noise

--- player/settings.py
+++ player/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
